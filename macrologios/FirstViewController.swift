e95db88c6c01f38148fe73b2ff583d6a59d3b250
Carmen Scholte Lubberink <cscholtelubberink@newnexusmobile.nl>
2019-08-06 09:40:55 +0200

    Setup app with three tabbed pages

---
+++ macrologios/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  macrologios
+//
+//  Created by Carmen Scholte Lubberink on 06/08/2019.
+//  Copyright © 2019 Carmen Scholte Lubberink. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ macrologios/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  macrologios
+//
+//  Created by Carmen Scholte Lubberink on 06/08/2019.
+//  Copyright © 2019 Carmen Scholte Lubberink. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
